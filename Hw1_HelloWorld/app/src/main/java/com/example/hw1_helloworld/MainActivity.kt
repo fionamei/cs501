6bec0251588d034d40224717ad6abcef6950c1e5
fionamei131@gmail.com <fionamei131@gmail.com>
2024-01-24 17:18:26 -0500

    pressing button changes text to hello world

--- Hw1_HelloWorld/app/src/main/java/com/example/hw1_helloworld/MainActivity.kt
+++ Hw1_HelloWorld/app/src/main/java/com/example/hw1_helloworld/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.hw1_helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var sayHelloBtn: Button
+    lateinit var helloText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sayHelloBtn = findViewById(R.id.sayHelloBtn)
+        helloText = findViewById(R.id.helloText)
+
+        sayHello()
+    }
+
+    fun sayHello() {
+        sayHelloBtn.setOnClickListener{
+            helloText.setText(R.string.hello_world)
+        }
     }
 }

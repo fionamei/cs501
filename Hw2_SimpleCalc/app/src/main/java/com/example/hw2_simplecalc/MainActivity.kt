0af7e21d4f04e209ea665676c31e3a456ec3c23a
fionamei131@gmail.com <fionamei131@gmail.com>
2024-01-31 20:27:50 -0500

    made xml stuff

---
+++ Hw2_SimpleCalc/app/src/main/java/com/example/hw2_simplecalc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hw2_simplecalc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

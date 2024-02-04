e66a737dde91bced9ca12a92e4c9e1b81d1e9364
fionamei131@gmail.com <fionamei131@gmail.com>
2024-02-04 15:29:50 -0500

    created gui calc proj

---
+++ Hw2_GUI_Calc/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Hw2_GUI_Calc"
+include(":app")
